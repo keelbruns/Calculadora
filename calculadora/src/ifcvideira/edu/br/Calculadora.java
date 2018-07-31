107630f9452eae1ceb2c6ea254a0ec27e2369e31
keelbruns <kellybrunskb@gmail.com>
2018-07-31 08:58:32 -0300

    nova classe

--- calculadora/src/ifcvideira/edu/br/Calculadora.java
+++ calculadora/src/ifcvideira/edu/br/Calculadora.java
@@ -4,7 +4,6 @@ public class Calculadora {
 	public int somar (int a, int b) {
 		return a + b;
 	}
-		
 	public int subtrair (int a, int b) {
 		return a - b;
 	}
---
+++ calculadora/src/ifcvideira/edu/br/Calculadora2.java
@@ -0,0 +1,5 @@
+package ifcvideira.edu.br;
+
+public class Calculadora2 {
+	Calculadora calc = new Calculadora();
+}
