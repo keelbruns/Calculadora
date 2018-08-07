2ec4cd96aa01077635c8370715cdc9fd9386e706
keelbruns <kellybrunskb@gmail.com>
2018-08-07 08:45:21 -0300

    interface

--- calculadora/src/ifcvideira/edu/br/Calculadora.java
+++ calculadora/src/ifcvideira/edu/br/Calculadora.java
@@ -1,16 +1,37 @@
 package ifcvideira.edu.br;
 
 public class Calculadora {
-	public int somar (int a, int b) {
-		return a + b;
+	private int a;
+	private int b;
+	public int resultado;
+	
+	public int somar() {
+		return resultado = a + b;
 	}
-	public int subtrair (int a, int b) {
-		return a - b;
+	public int subtrair() {
+		return resultado = a - b;
 	}
-	public int dividir (int a, int b) {
-		return a / b;
+	public int dividir() {
+		return resultado = a / b;
 	}
-	public int multiplicação (int a, int b) {
-		return a * b;
+	public int multiplicacao() {
+		return resultado = a * b;
 	}
+	
+	
+	
+	
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	public int getB() {
+		return b;
+	}
+	public void setB(int b) {
+		this.b = b;
+	}
+
 }
--- calculadora/src/ifcvideira/edu/br/Calculadora2.java
+++ calculadora/src/ifcvideira/edu/br/Calculadora2.java
@@ -1,5 +1,42 @@
 package ifcvideira.edu.br;
 
+import javax.swing.JOptionPane;
+
 public class Calculadora2 {
-	Calculadora calc = new Calculadora();
+	public static void main(String[] args) {
+		Calculadora cal = new Calculadora();
+		int op;
+		cal.setA(Integer.parseInt(JOptionPane.showInputDialog("Digite um valor:")));
+		cal.setB(Integer.parseInt(JOptionPane.showInputDialog("Digite um valor:")));
+		
+		for(;;){
+			op=Integer.parseInt(JOptionPane.showInputDialog("Escolha uma opção:\n\n"
+					+"1. Somar \n"
+					+"2. Subtrair\n"
+					+"3. Dividir \n"
+					+"4. Multiplicar \n"
+				    +"5. Sair"));
+			
+			if(op==1){
+				int resultado = cal.somar();
+				JOptionPane.showMessageDialog(null, "Resultado: "+resultado);
+			}else  if(op==2){
+				int resultado = cal.subtrair();
+				JOptionPane.showMessageDialog(null, "Resultado: "+resultado);
+			}else  if(op==3){
+				int resultado = cal.dividir();
+				JOptionPane.showMessageDialog(null, "Resultado: "+resultado);
+			}else  if(op==4){
+				int resultado = cal.multiplicacao();
+				JOptionPane.showMessageDialog(null, "Resultado: "+resultado);
+			}else if(op==5){
+				JOptionPane.showMessageDialog(null, "Finalizado");
+				break;
+			}else{
+				JOptionPane.showMessageDialog(null, "Opção inválida");
+			}
+		}
+	}
 }
+	
+
